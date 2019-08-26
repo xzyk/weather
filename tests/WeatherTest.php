a83f1ab5ee6e0f9e42e1c662d2bc4ebe0bf2d2ea
yaokai <15549070665@163.com>
2019-08-26 18:37:08 +0800

    注释

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the xzyk/weather.
  *
- * (c) overtrue <15549070665@163.com>
+ * (c) xzyk <15549070665@163.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
